d4bcdd43c1fe22f534aa285ee9842ba6f8203658
Michal <michalo080801@gmail.com>
2022-10-22 16:50:12 +0200

    Adding project

---
+++ app/src/main/java/com/example/sm_3/TaskListActivity.java
@@ -0,0 +1,13 @@
+package com.example.sm_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class TaskListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
---
+++ app/src/main/java/com/example/sm_3/TaskStorage.java
@@ -0,0 +1,45 @@
+package com.example.sm_3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class TaskStorage {
+    private static TaskStorage instance;
+
+    private final List<Task> tasks;
+
+
+    public static TaskStorage getInstance(){
+        if(instance == null)
+            instance = new TaskStorage();
+
+        return instance;
+    }
+
+    private TaskStorage(){
+        tasks = new ArrayList<>();
+        for(int i =1;i <=150;i++){
+            Task task = new Task();
+            task.setName("Pilne zadanie numer " + i);
+            task.setDone(i%3 == 0);
+            tasks.add(task);
+        }
+    }
+
+    public List<Task> getTasks(){
+        return tasks;
+    }
+
+    public Task getTask(UUID taskId) {
+        for (Task task : tasks) {
+            if (taskId.equals(task.getId()))
+                return task;
+            }
+            return null;
+        }
+}
+
+
+
